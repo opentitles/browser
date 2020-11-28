46863020c92a388bf591f5b0de90384b8d89c156
Floris de Bijl <18357629+Fdebijl@users.noreply.github.com>
2020-11-28 16:49:07 +0100

    fix: launch headless

--- src/web/titleFetch.ts
+++ src/web/titleFetch.ts
@@ -11,7 +11,7 @@ let success = 0;
 
 export const browserInit = async (): Promise<void> => {
   browser = await puppeteer.launch({
-    headless: false,
+    headless: true,
     ignoreHTTPSErrors: true,
     args: ['--no-sandbox', '--disable-setuid-sandbox']
   });
